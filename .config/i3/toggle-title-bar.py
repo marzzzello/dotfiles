f89137a2a48dc68389fec2a2513b9e3b3abbcb29
marzzzello <15198431+marzzzello@users.noreply.github.com>
2019-05-31 10:30:38 +0200

    fix filter

--- .config/i3/toggle-title-bar.py
+++ .config/i3/toggle-title-bar.py
@@ -1,11 +1,2 @@
 #!/usr/bin/env python
 # This file will get overwritten by wpgtk because there is a template file under ~/.config/wpg/templates/
-
-import i3
-
-border = i3.filter(focused=True)[0]['border']
-
-if border == "normal":
-  i3.command("border pixel 5")
-else:
-  i3.command("border normal 5")
