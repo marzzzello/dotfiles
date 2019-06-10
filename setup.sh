3a3707e0c7f9c61fa9e7d6567e66b11f5a6f6b9b
marzzzello <15198431+marzzzello@users.noreply.github.com>
2019-06-10 16:03:10 +0200

    add bash highlighting to README and add setup.sh as extra file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+git clone --recursive  --bare https://gitlab.com/marzzzello/dotfiles.git $HOME/.dotfiles
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} dirname .dotfiles-backup/{} | xargs -I{} mkdir -p {}
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
