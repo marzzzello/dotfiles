30828bb76a6d754c74aeb90c5e1c7209301d1565
marzzzello <15198431+marzzzello@users.noreply.github.com>
2019-08-31 16:30:07 +0200

    add hybrid suspend

--- .config/i3/quit-i3.zsh
+++ .config/i3/quit-i3.zsh
@@ -6,18 +6,20 @@ Logout
 Reboot
 Power off
 Suspend
-Hibernate"
+Hibernate
+Hibernate and suspend"
 
 selection=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "power-menu")
 lock=(light-locker-command -l)
 
 case "$selection" in
-    Lock)         $lock;;
-    Logout)       i3-msg exit;;
-    Reboot)       reboot;;
-    "Power off")  poweroff;;
-    Suspend)      $lock; systemctl suspend;;
-    Hibernate)    $lock; systemctl hibernate;;
-    *)            echo "what is that?";;
+    Lock)                       $lock;;
+    Logout)                     i3-msg exit;;
+    Reboot)                     reboot;;
+    "Power off")                poweroff;;
+    Suspend)                    $lock; systemctl suspend;;
+    Hibernate)                  $lock; systemctl hibernate;;
+    "Hibernate and suspend")    $lock; systemctl hybrid-sleep;;
+    *)                          echo "what is that?";;
 esac
 exit 0
