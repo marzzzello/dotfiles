730b1436bc097cb9d790b50baa3441c66d15159a
marzzzello <15198431+marzzzello@users.noreply.github.com>
2020-01-18 18:20:08 +0100

    scripted themes are only supported for i3/sway

--- .config/wpg/scripted_themes/cron.sh
+++ .config/wpg/scripted_themes/cron.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
 # Useful for logs
-date +%FT%T%:z;
+
+DESKTOP=$(loginctl show-session 2 -p Desktop | awk -F= '{print $2}')
+
+date +%FT%T%:z
 
 source $HOME/.config/wpg/scripted_themes/theme_func.sh
 
-renew
+case $choice in
+  i3|sway) renew;;
+  *)       echo "Desktop not supported";;
+esac
+
