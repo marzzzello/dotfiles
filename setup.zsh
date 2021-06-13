033c6356cf057337ce83d757df99ba069fb2cbbe
marzzzello <15198431+marzzzello@users.noreply.github.com>
2021-06-13 13:44:01 +0200

    improve setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -1,23 +1,25 @@
 #!/usr/bin/env zsh
-cd "${HOME}" || exit
-git clone --recursive --jobs 8 --bare https://gitlab.com/marzzzello/dotfiles.git "${HOME}/.git"
-alias dotfiles="git --work-tree=${HOME}"
+cd "$HOME" || exit
+git clone --recursive --jobs 8 --bare https://gitlab.com/marzzzello/dotfiles.git "$HOME/.git"
 
 # backup
-mkdir -p .dotfiles-backup
-if dotfiles checkout; then
+BACKUP_DIR=".dotfiles-backup"
+mkdir -p "$BACKUP_DIR"
+mv .antigen "$BACKUP_DIR" &>/dev/null
+if git --work-tree="$HOME" checkout &>/dev/null; then
   echo "Checked out dotfiles."
 else
-  echo "Backing up pre-existing dot files."
-  dotfiles checkout 2>&1 | awk -F '\t' '/\t/ {print $2}' | xargs -I{} dirname .dotfiles-backup/{} | xargs -I{} mkdir -p {}
-  dotfiles checkout 2>&1 | awk -F '\t' '/\t/ {print $2}' | xargs -I{} mv {} .dotfiles-backup/{}
+  git --work-tree="$HOME" checkout 2>&1 | awk -F '\t' '/\t/ {print $2}' >"$BACKUP_DIR/BACKUP_FILE_LIST"
+  echo "Backing up $(wc -l <$BACKUP_DIR/BACKUP_FILE_LIST) pre-existing dot files."
+  xargs -I{} dirname $BACKUP_DIR/{} <"$BACKUP_DIR/BACKUP_FILE_LIST" | xargs -I{} mkdir -p {}
+  xargs -I{} mv {} $BACKUP_DIR/{} <"$BACKUP_DIR/BACKUP_FILE_LIST"
 fi
 
-dotfiles config core.bare false
+git --work-tree="$HOME" config core.bare false
 git checkout
 git submodule update --init --recursive --jobs 8
 
-rm "${HOME}/.git/config"
+rm "$HOME/.git/config"
 git config include.path ../.gitconfig.local
 
 if command -v wpg-install.sh >/dev/null; then
@@ -29,13 +31,13 @@ else
 fi
 
 wallpaper="wallpaper-forest-4k.jpg"
-echo "Downloading wallpaper to ${wallpaper}"
-curl -L https://upload.wikimedia.org/wikipedia/commons/6/69/7-78049_small-memory-8k-wallpaper-forest-minimalist-wallpaper-4k.jpg --output "${HOME}/${wallpaper}"
+echo "Downloading wallpaper to $wallpaper"
+curl -L https://upload.wikimedia.org/wikipedia/commons/6/69/7-78049_small-memory-8k-wallpaper-forest-minimalist-wallpaper-4k.jpg --output "$HOME/$wallpaper"
 if command -v wpg >/dev/null; then
   echo "Add and set colorscheme"
-  wpg -a ${wallpaper}
-  wpg -A ${wallpaper}
-  wpg -s ${wallpaper}
+  wpg -a $wallpaper
+  wpg -A $wallpaper
+  wpg -s $wallpaper
 else
   echo "Run wpg to generate the config from the template files"
 fi
