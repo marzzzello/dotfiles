49ff076688f1fc5d1f57369a6af09711d07834d8
marzzzello <15198431+marzzzello@users.noreply.github.com>
2021-06-23 21:17:27 +0200

    add lock.py

---
+++ .config/i3/lock.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# modified from original:
+# https://github.com/madnight/dotfiles/blob/5ba9c19107478cd07fc5eb36a815c54d51105763/scripts/lock.py
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+import dbus
+import subprocess
+import sys
+import threading
+
+from PIL import Image
+import mss
+
+import time
+
+
+class Locker:
+    def __init__(self):
+        self.locked = False
+        self.notifications_props = None
+
+    def set_notifications_props(self):
+        if self.notifications_props is None:
+            notifications = dbus.SessionBus().get_object(
+                'org.freedesktop.Notifications', '/org/freedesktop/Notifications'
+            )
+            self.notifications_props = dbus.Interface(notifications, 'org.freedesktop.DBus.Properties')
+
+    def run(self):
+        DBusGMainLoop(set_as_default=True)
+        bus = dbus.SystemBus()
+        # register your signal callback
+        bus.add_signal_receiver(
+            self.signal_handler,
+            # bus_name='org.freedesktop.DBus',
+            dbus_interface='org.freedesktop.login1.Session',
+            # destination_keyword='destination',
+            # interface_keyword='interface',
+            member_keyword='member',
+            message_keyword='message',
+            path_keyword='path',
+            # sender_keyword='sender',
+        )
+
+        loop = GLib.MainLoop()
+        loop.run()
+
+    def signal_handler(self, *args, **kwargs):
+        if kwargs['member'] == 'Lock':
+            self.lock()
+        elif kwargs['member'] == 'Unlock':
+            self.unlock()
+
+    def i3lock(self, result):
+        print('LOCKED')
+        self.locked = True
+        self.killprog = 'i3lock'
+        subprocess.run(
+            ['i3lock', '--raw', str(result.width) + 'x' + str(result.height) + ':rgb', '-i', '/dev/stdin'],
+            stdout=subprocess.PIPE,
+            input=result.tobytes(),
+        )
+        self.unlock(kill=False)
+
+    def betterlockscreen(self):
+        print('LOCKED')
+        self.locked = True
+        self.killprog = 'i3lock'
+        subprocess.run(['betterlockscreen', '--lock'], stdout=subprocess.PIPE)
+        self.unlock(kill=False)
+
+    def unlock(self, kill=True):
+        if self.locked is False:
+            return
+
+        if kill is True:
+            subprocess.run(['killall', self.killprog], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        print('UNLOCKED')
+        self.locked = False
+
+        subprocess.run(['loginctl', 'unlock-session'], stdout=subprocess.PIPE)
+
+        # if notifications have been on then resume them
+        if self.paused_before is False:
+            self.notifications_props.Set('org.dunstproject.cmd0', 'paused', False)
+            print('resumed notifications')
+
+    def lock(self):
+        if self.locked:
+            return
+
+        sct = mss.mss()
+
+        # take screenshot
+        sct_img = sct.grab(sct.monitors[0])
+        img = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+
+        # pixelate
+        imgSmall = img.resize((int(img.size[0] / 10), int(img.size[1] / 10)), resample=Image.BILINEAR)
+        result = imgSmall.resize(img.size, Image.NEAREST)
+
+        # add lock icon
+        try:
+            icon_path = sys.argv[1]
+        except IndexError:
+            print('No lock icon path set')
+            # icon_path = '/usr/share/i3lock-fancy-dualmonitor/lock.png'
+        else:
+            icon = Image.open(icon_path)
+
+            # show lock icon on all monitors
+            for monitor in sct.monitors[1:]:
+                area = (
+                    int(((monitor['width'] / 2) + monitor['left']) - icon.size[0] / 2),
+                    int(((monitor['height'] / 2) + monitor['top']) - icon.size[1] / 2),
+                )
+                # add lock icon to bg image
+                result.paste(icon, area, icon)
+
+        self.set_notifications_props()
+        self.paused_before = bool(self.notifications_props.GetAll('org.dunstproject.cmd0')['paused'])
+
+        # if notifications are on then pause them
+        if self.paused_before is False:
+            self.notifications_props.Set('org.dunstproject.cmd0', 'paused', True)
+            print('paused notifications')
+
+        threading.Thread(target=self.i3lock, args=(result,)).start()
+        # threading.Thread(target=self.betterlockscreen).start()
+
+
+import sys
+
+try:
+
+    print(time.asctime(), 'Starting', flush=True)
+
+    locker = Locker()
+    try:
+        locker.run()
+    except KeyboardInterrupt:
+        pass
+
+    print('\nExiting...', flush=True)
+
+
+except (BrokenPipeError, IOError):
+    print('BrokenPipeError caught', file=sys.stderr)
+
+print('Done', file=sys.stderr)
+sys.stderr.close()
