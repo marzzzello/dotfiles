0da71435ae2ff14cad5b338b527b475a5e2f93c2
marzzzello <15198431+marzzzello@users.noreply.github.com>
2022-10-22 14:57:08 +0200

    use old dropdown script for i3 with kitty

--- .config/i3/dropdown-terminal.py
+++ .config/i3/dropdown-terminal.py
@@ -2,10 +2,10 @@
 from i3ipc import Connection
 
 conn = Connection()
-terms = conn.get_tree().find_instanced('gnome-terminal-server')
+terms = conn.get_tree().find_instanced('kitty')
 for term in terms:
     if term.parent.scratchpad_state != 'none':
         conn.command('scratchpad show')
         exit(0)
 
-conn.command('exec --no-startup-id "gnome-terminal --window --title terminal-dropdown"')
+conn.command('exec --no-startup-id "kitty --title terminal-dropdown"')
