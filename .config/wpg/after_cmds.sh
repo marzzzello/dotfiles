18cbe107b9059a1b9cc17e5f8451f21425cce615
marzzzello <15198431+marzzzello@users.noreply.github.com>
2023-04-01 13:19:45 +0200

    wpgtk: reapply old theme

--- .config/wpg/after_cmds.sh
+++ .config/wpg/after_cmds.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #makoctl reload
 killall --quiet i3bar && i3bar --bar_id=bar-0
+THEME=$(gsettings get org.gnome.desktop.interface gtk-theme)
 gsettings set org.gnome.desktop.interface gtk-theme ""
 sleep 0.2
-gsettings set org.gnome.desktop.interface gtk-theme FlatColor
+gsettings set org.gnome.desktop.interface gtk-theme "${THEME}"
